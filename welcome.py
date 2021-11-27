ff8d88520cf6fa4f290f948ab9edbce826c03549
AbuMahfuz <mahfuz2_1999@yahoo.com>
2021-11-27 18:33:26 -0500

    Rahma Ruhama

--- welcome.py
+++ welcome.py
@@ -1,1 +1,84 @@
-print("hello Rahma, Ruhama and Saleh")
+
+
+Rahma_Age = 7
+Ruhama_Age = 5
+Age_at_Kindergarten = 5
+Age_at_2grade = 7
+
+print(Ruhama_Age == Age_at_Kindergarten)
+print(Rahma_Age == Age_at_Kindergarten)
+print(Age_at_2grade == Rahma_Age)
+
+if Rahma_Age < Age_at_2grade:
+    print("Rahma should not be in Kindergarten")
+elif Rahma_Age > Age_at_2grade:
+    print("Rahma should not be in 2nd grade")
+else:
+    print("Rahma should be in 2nd grade")
+
+print("####################################")
+print("####################################")
+print("          ")
+print("Ruhama")
+print("          ")
+print("          ")
+print("          ")
+print("          ")
+
+#print("Hello Rahma, Ruhama and Saleh")
+#print("How are you doing?")
+#print("Rahma said good, then Ruhama also said good")
+#print("But Saleh did not say anything")
+#print("Because he is in Michigan, and he does not know what we are talking about")
+#print("Rahma and Ruhama said, we need to clean up our dad and moms room")
+#print("Rahma  Ruhama are happy about their parents")
+
+
+
+Rahma_Age = 7
+Ruhama_Age = 5
+Age_at_Kindergarten = 5
+Age_at_2grade = 7
+
+print(Ruhama_Age == Age_at_Kindergarten)
+print(Rahma_Age == Age_at_Kindergarten)
+print(Age_at_2grade == Rahma_Age)
+
+if Ruhama_Age != Age_at_Kindergarten:
+    print("Ruhama should not be in 2nd grade")
+elif Ruhama_Age > Age_at_Kindergarten:
+    print("Ruhama should not be in Kindergarten")
+else:
+    print("Ruhama should be in Kindergarten")
+
+
+print("Hello Rahma, Ruhama and Saleh")
+print("How are you doing?")
+print("Rahma said good, then Ruhama also said good")
+print("But Saleh did not say anything")
+print("Because he is in Michigan, and he does not know what we are talking about")
+print("Rahma and Ruhama said, we need to clean up our dad and moms room")
+print("Rahma  Ruhama are happy about their parents")
+
+print (4==4)
+print(5==5)
+print(5!=4)
+print(5>=4)
+########
+print("####################################")
+print("####################################")
+print("          ")
+print("          ")
+print("New Programming")
+print("          ")
+print("          ")
+print("          ")
+
+
+def print_rahma():
+    text = "Rama and Ruhama are very smart"
+    print(text)
+    print(text)
+    print(text)
+print_rahma()
+
