d897b3e4ea7f69bfa1577a83c82c8ea2e65cf504
AbuMahfuz <mahfuz2_1999@yahoo.com>
2021-11-27 16:48:38 -0500

    November 27

--- welcome.py
+++ welcome.py
@@ -5,3 +5,18 @@ print("But Saleh did not say anything")
 print("Because he is in Michigan, and he does not know what we are talking about")
 print("Rahma and Ruhama said, we need to clean up our dad and moms room")
 print("Rahma and Ruhama are happy about their parents")
+
+print (5==4)
+print(5==5)
+print(5!=4)
+print(5>=4)
+print(5<=4)
+
+Rahma_Age = 7
+Ruhama_Age = 5
+Age_at_Kindergarten = 5
+Age_at_2grade = 7
+
+print(Ruhama_Age == Age_at_Kindergarten)
+print(Rahma_Age == Age_at_Kindergarten)
+print(Rahma_Age == Age_at_2grade)
