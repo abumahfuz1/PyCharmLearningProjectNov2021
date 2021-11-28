6d21faabf01320e39f14b7fd94ee1c1adfad89b6
AbuMahfuz <mahfuz2_1999@yahoo.com>
2021-11-27 20:26:06 -0500

    Save before new

--- welcome.py
+++ welcome.py
@@ -12,3 +12,12 @@ def school_age_calculator(age,name):
     else:
         print("they gow up so fast!")
 school_age_calculator(7, "Rahma")
+
+
+def add_ten_to_age(age):
+    new_age = age +10
+    return new_age
+
+How_Old_Will_I_Be = add_ten_to_age(20)
+print(How_Old_Will_I_Be)
+
