c15c5aa79c64390ab4d19dcfcdddf7f70608d3b0
AbuMahfuz <mahfuz2_1999@yahoo.com>
2021-11-27 20:42:19 -0500

    Save before new

--- welcome.py
+++ welcome.py
@@ -13,7 +13,12 @@ def school_age_calculator(age,name):
         print("they gow up so fast!")
 school_age_calculator(7, "Rahma")
 
-
+print("####################################")
+print("####################################")
+print("          ")
+print("MyAge")
+print("          ")
+####My Age
 def add_ten_to_age(age):
     new_age = age +10
     return new_age
@@ -21,3 +26,20 @@ def add_ten_to_age(age):
 How_Old_Will_I_Be = add_ten_to_age(20)
 print(How_Old_Will_I_Be)
 
+print("####################################")
+print("####################################")
+print("          ")
+print("While Loop")
+print("          ")
+x=0
+while (x<5):
+    print(x)
+    x=x+1
+
+    #For
+    for x in range(5,10):
+        print(x)
+
+        days=["Mon", "Tue", "Wed", "Thr", "Fri", "Sat", "Sun"]
+        for d in days:
+            print(d)
