2d70dfdc0e3b34aa4667cfbcee65af84f6bb832f
AbuMahfuz <mahfuz2_1999@yahoo.com>
2021-11-27 20:03:15 -0500

    Save before new

--- welcome.py
+++ welcome.py
@@ -1,84 +1,11 @@
 
-
-Rahma_Age = 7
-Ruhama_Age = 5
-Age_at_Kindergarten = 5
-Age_at_2grade = 7
-
-print(Ruhama_Age == Age_at_Kindergarten)
-print(Rahma_Age == Age_at_Kindergarten)
-print(Age_at_2grade == Rahma_Age)
-
-if Rahma_Age < Age_at_2grade:
-    print("Rahma should not be in Kindergarten")
-elif Rahma_Age > Age_at_2grade:
-    print("Rahma should not be in 2nd grade")
-else:
-    print("Rahma should be in 2nd grade")
-
-print("####################################")
-print("####################################")
-print("          ")
-print("Ruhama")
-print("          ")
-print("          ")
-print("          ")
-print("          ")
-
-#print("Hello Rahma, Ruhama and Saleh")
-#print("How are you doing?")
-#print("Rahma said good, then Ruhama also said good")
-#print("But Saleh did not say anything")
-#print("Because he is in Michigan, and he does not know what we are talking about")
-#print("Rahma and Ruhama said, we need to clean up our dad and moms room")
-#print("Rahma  Ruhama are happy about their parents")
+def school_age_calculator(age,name):
 
 
-
-Rahma_Age = 7
-Ruhama_Age = 5
-Age_at_Kindergarten = 5
-Age_at_2grade = 7
-
-print(Ruhama_Age == Age_at_Kindergarten)
-print(Rahma_Age == Age_at_Kindergarten)
-print(Age_at_2grade == Rahma_Age)
-
-if Ruhama_Age != Age_at_Kindergarten:
-    print("Ruhama should not be in 2nd grade")
-elif Ruhama_Age > Age_at_Kindergarten:
-    print("Ruhama should not be in Kindergarten")
-else:
-    print("Ruhama should be in Kindergarten")
-
-
-print("Hello Rahma, Ruhama and Saleh")
-print("How are you doing?")
-print("Rahma said good, then Ruhama also said good")
-print("But Saleh did not say anything")
-print("Because he is in Michigan, and he does not know what we are talking about")
-print("Rahma and Ruhama said, we need to clean up our dad and moms room")
-print("Rahma  Ruhama are happy about their parents")
-
-print (4==4)
-print(5==5)
-print(5!=4)
-print(5>=4)
-########
-print("####################################")
-print("####################################")
-print("          ")
-print("          ")
-print("New Programming")
-print("          ")
-print("          ")
-print("          ")
-
-
-def print_rahma():
-    text = "Rama and Ruhama are very smart"
-    print(text)
-    print(text)
-
-print_rahma()
-
+    if age < 5:
+        print("Enjoy the time!", name, "is only", age)
+    elif age == 5:
+        print("Ejnoy Kindergarten,", name)
+    else:
+        print("they gow up so fast!")
+school_age_calculator(3, "Rahma")
