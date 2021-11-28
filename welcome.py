b1392a5cde85505e3d7b5e677b5782bf1de786c7
AbuMahfuz <mahfuz2_1999@yahoo.com>
2021-11-28 09:41:25 -0500

    Variable

--- welcome.py
+++ welcome.py
@@ -1,12 +1,12 @@
 
-borozi_name = "Rahma"
-sister_name = "Ruhama"
-rahma_age = 7
-ruhama_age = 5
+character_name = "Rahma"
+#sister_name = "Ruhama"
+character_age = 7
+#ruhama_age = 5
 
-print("Amar boro zi name " + borozi_name + ",")
-print("Rahma is " + rahma_age + "years old,")
+print("Amar boro zi name " + character_name + ",")
+print("Rahma is" + character_age + " years old,")
 print("She is very smart")
 print("Rahma has a baby sister")
 print("Her name is Ruhama")
-print("Ruhama is " +rahma_age + "years old")
+#print("Ruhama is " +ruhama_age + "years old")
