c02f869e08731b3e57c1b08c600ab9099582c93f
AbuMahfuz <mahfuz2_1999@yahoo.com>
2021-11-28 08:39:55 -0500

    Draw

--- welcome.py
+++ welcome.py
@@ -1,19 +1,16 @@
+print("          /\|")
+print("         /  \|")
+print("        /    \|")
+print("       /      \|")
+print("      /        \|")
+print("     /__________\|")
+print("    /            \|")
 
+print("                /\|")
+print("    |    /  \|")
+print("    |   /    \|")
+print("    |  /      \|")
+print("    | /        \|")
+print("    |/          \|")
+print("    |            \|")
 print("    #####      ")
-print("While Loop")
-print("          ")
-x=0
-while (x<5):
-    print(x)
-    x=x+1
-
-    #For
-    for x in range(5,10):
-        print(x)
-
-print("  ######        ")
-
-days=["Mon", "Tue", "Wed", "Thr", "Fri", "Sat", "Sun"]
-for d in days:
-            if (d=="Thr"):break
-            print(d)
