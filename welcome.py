a25a39daf513b33b29645127276b7dd7701f8654
AbuMahfuz <mahfuz2_1999@yahoo.com>
2021-11-28 08:01:25 -0500

    Save before new

--- welcome.py
+++ welcome.py
@@ -1,34 +1,5 @@
 
-def school_age_calculator(age,name):
-
-
-    if age < 5:
-        print("Enjoy the time!", name, "is only", age)
-    elif age == 5:
-        print("Ejnoy Kindergarten,", name)
-    elif age ==7:
-        print("Enjoy second grade!")
-
-    else:
-        print("they gow up so fast!")
-school_age_calculator(7, "Rahma")
-
-print("####################################")
-print("####################################")
-print("          ")
-print("MyAge")
-print("          ")
-####My Age
-def add_ten_to_age(age):
-    new_age = age +10
-    return new_age
-
-How_Old_Will_I_Be = add_ten_to_age(20)
-print(How_Old_Will_I_Be)
-
-print("####################################")
-print("####################################")
-print("          ")
+print("    #####      ")
 print("While Loop")
 print("          ")
 x=0
@@ -40,6 +11,9 @@ while (x<5):
     for x in range(5,10):
         print(x)
 
-        days=["Mon", "Tue", "Wed", "Thr", "Fri", "Sat", "Sun"]
-        for d in days:
+print("  ######        ")
+
+days=["Mon", "Tue", "Wed", "Thr", "Fri", "Sat", "Sun"]
+for d in days:
+            if (d=="Thr"):break
             print(d)
