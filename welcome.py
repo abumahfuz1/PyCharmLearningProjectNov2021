7f082fdd6f9b264e33255d376f9be4d3cca5dd40
AbuMahfuz <mahfuz2_1999@yahoo.com>
2021-11-28 09:34:36 -0500

    Variable

--- welcome.py
+++ welcome.py
@@ -1,16 +1,12 @@
-print("          /\|")
-print("         /  \|")
-print("        /    \|")
-print("       /      \|")
-print("      /        \|")
-print("     /__________\|")
-print("    /            \|")
 
-print("                /\|")
-print("    |    /  \|")
-print("    |   /    \|")
-print("    |  /      \|")
-print("    | /        \|")
-print("    |/          \|")
-print("    |            \|")
-print("    #####      ")
+borozi_name = "Rahma"
+sister_name = "Ruhama"
+rahma_age = 7
+ruhama_age = 5
+
+print("Amar boro zi name " + borozi_name + ",")
+print("Rahma is " + rahma_age + "years old,")
+print("She is very smart")
+print("Rahma has a baby sister")
+print("Her name is Ruhama")
+print("Ruhama is " +rahma_age + "years old")
