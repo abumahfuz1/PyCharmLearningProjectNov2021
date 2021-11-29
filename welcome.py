d5d5740016d60d1ebf5246a1220cf35d631476dd
AbuMahfuz <mahfuz2_1999@yahoo.com>
2021-11-28 22:45:45 -0500

    List

--- welcome.py
+++ welcome.py
@@ -1,44 +1,4 @@
-
-borozi_name = "Rahma"
-sister_name = "Ruhama"
-borozi_age = "7"
-sister_age = "5"
-
-print("Amar boro zi name " + borozi_name + ",")
-print("Rahma is " + borozi_age + " years old,")
-print("She is very smart")
-print("Rahma has a baby sister")
-print("Her name is Ruhama")
-print("Ruhama is " +sister_age + " years old")
-
-
-print("Al-Ikhlas \nTraining \nAcademy")
-
-print("Al-Ikhlas \"Training Academy")
-
-print("---- Asigning Value------")
-
-media = "Banglaamar Maltimedia"
-print(media + " was established in 2002")
-print(media.upper())
-print(media.lower())
-print(media.upper().isupper())
-print(media.lower().isupper())
-print(len(media))
-print(media[0])
-
-
-print("------ Concatination------")
-print("------ Input-----")
-
-
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-print("Hello " + name + "! You are " + age )
-
-print("------ Input Calculator-----")
-
-num1 = input("Enter a number: ")
-num2 = input("Enter another number: ")
-result = int(num1) + int(num2)
-print(result)
+print("  ### LIst ###   ")
+friends = ["Kevin", "Karen", "Jim"]
+print(friends)
+print(friends[2])
