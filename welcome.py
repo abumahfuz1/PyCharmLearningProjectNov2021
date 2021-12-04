61749ffeea0dbcc6952851721079684d507f39dd
AbuMahfuz <mahfuz2_1999@yahoo.com>
2021-12-04 18:42:05 -0500

    List

--- welcome.py
+++ welcome.py
@@ -10,8 +10,8 @@ print( "Multiple line comments")
 print( "Just write comments between '''    '''")
 
 '''
+Commented out code
 helllo
 these are just comment lines, should
 should not be executed
-
 '''
