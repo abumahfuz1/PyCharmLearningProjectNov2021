53b173cb2d955b244bc9d68145a7118520eb6420
AbuMahfuz <mahfuz2_1999@yahoo.com>
2021-12-04 16:36:07 -0500

    List

--- welcome.py
+++ welcome.py
@@ -1,49 +1,18 @@
-print("  ### LIst ###   ")
-friends = ["Kevin", "Karen", "Jim", "Abu", "Mahfuz"]
-print(friends)
-#print(friends[1:3])
-#print(friends[1:2])
-
-friends[0] = "Saleh" #replaces value 0 Kevin to Saleh
-print(friends[0])
-print(friends[2])
-print(friends[-1])
-
-print("  ### List ###   ")
-lucky_numbers = [45, 8, 15, 16, 23, 42]
-friends = ["Kevin", "Karen", "Jim", "Abu", "Abu", "Mahfuz"]
-
-friends.append("Abbu")
-print(friends)
-
-friends.insert(6, "Abdullah")
-print(friends)
-friends.reverse()
+coordinates = [(4,5), (6,7), (80, 34)]
+coordinates[1] = 10
+print(coordinates [1])
 
 
-print(friends)
-print(lucky_numbers)
-lucky_numbers.reverse()
-print(lucky_numbers)
-friends.remove("Abdullah")
-print(friends)
-
-#lucky_numbers.clear()
-print(friends)
-
-lucky_numbers.sort()
-print(lucky_numbers)
-lucky_numbers.reverse()
-print(lucky_numbers)
-
-friends.extend(lucky_numbers)
-print(friends)
-#lucky_numbers.clear()
-#friends.clear()
-print(friends)
-
-friends.pop() #removes last element
-print(friends)
-
-print((friends.index("Kevin")))
-print(friends.count("Abu"))
+print("#### Functions  ######")
+def translate(phrase):
+    translation = " "
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + " G "
+            else:
+                translation = translation + letter + "g"
+        else:
+            translation = translation + letter
+    return translation
+print((translate((input("Enter a phrase: ")))))
