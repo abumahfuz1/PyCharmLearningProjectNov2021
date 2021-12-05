6be17c7ec52ab79625ee9466284d9937ec9768d6
AbuMahfuz <mahfuz2_1999@yahoo.com>
2021-12-05 13:09:48 -0500

    List

--- welcome.py
+++ welcome.py
@@ -7,7 +7,11 @@ employee_file.close()
 #print(employee_file.read())
 
 
+employee_file = open("index.html", "w") #to replace or re write on the older file use w
 
+employee_file.write("<p>This is HTML Code</p>")
+
+employee_file.close()
 
     #reader = csv.reader(f)
     #for row in reader:
